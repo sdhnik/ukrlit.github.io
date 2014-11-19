af64a5491e9a9111ac6dd438d9440f5353dfc72d
sdhnik <sdhnik@gmail.com>
2014-11-19 22:44:00 +0200

    fix links

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
 	var widthBlock = $('.content-block').width();
-	$("ul.multimedia-portfolio").multimedia_portfolio({width: widthBlock + 15, baseDir: '.', nbelem: 4});
+	$("ul.multimedia-portfolio").multimedia_portfolio({width: widthBlock + 15, nbelem: 4});
 });
