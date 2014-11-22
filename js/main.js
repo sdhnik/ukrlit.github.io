f882134744c0573bc6b46db8dba9e302f89977fb
sdhnik <sdhnik@gmail.com>
2014-11-22 22:43:12 +0200

    fix files

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
-	var widthBlock = $('.content-block').width();
-	$("ul.multimedia-portfolio").multimedia_portfolio({width: widthBlock + 15, nbelem: 4});
+	var widthBlock = $('html').width();
+	$("ul.multimedia-portfolio").multimedia_portfolio({width: widthBlock, nbelem: 4});
+	$(".portfolio-container").niceScroll({cursorborder:"", cursorcolor:"#1f96c3", touchbehavior: true, autohidemode: false, cursordragontouch: true});
 });
--- js/vendor/jquery.multimedia-portfolio-2.0.js
+++ js/vendor/jquery.multimedia-portfolio-2.0.js
@@ -6,14 +6,13 @@
 // Copyright (c) 2007-2010 OpenStudio
 // licence : GPL
 ========================================================= */
-(function($) {
 
+(function($) {
 $.fn.multimedia_portfolio = function(options) {
 	this.each(function(){
 	if($(window).width() > 980) {
 		var uniqueID = new Date();
 		var rel_id=uniqueID.getTime();
-		var mousewheelposition = 0;
 		var defaultwidth = 320, defaultheight = 210;
 		var jsFolder = "/ukrlit.github.io/js/vendor/";
 		$(this).wrap("<div class='portfolio-container'></div>");
@@ -27,16 +26,16 @@ $.fn.multimedia_portfolio = function(options) {
 		var elements = $(this).children().not('.portfolio-loading-bar');
 		var borderwidth = parseInt(((settings.width)/900)*7);
 		var titlesize = (def_element_width/366);
-		$('.portfolio-container').prepend("<div class='portfolio-bg-left'>&nbsp;</div><div class='portfolio-bg-right'>&nbsp;</div>").append("<div class='masque-left'>&nbsp;</div><div class='masque-right'>&nbsp;</div>");
-		if (elements.length > settings.nbelem) $('.portfolio-container').append("<div class='portfolio-bg-bottom-left'>&nbsp;</div><div class='portfolio-bg-bottom-right'>&nbsp;</div>");
 		var ratio_largeur = ((elements.length*def_element_width - settings.width) / (elements.length*def_element_width));
+
+		$(this).css('width', def_element_width*elements.length);
 		$(".portfolio-container").css("width", settings.width+'px');
 		$(".portfolio-container").css("height", portfolio_height+'px');
 		$(".portfolio-container").css("margin-top", -(portfolio_height / 2) + 12);
-		if (elements.length > settings.nbelem) $(".multimedia-portfolio").addClass("border-bottom-blue");
+
 		for ( var i = 0; i < elements.length; i++ ) {
 				$(elements[i]).css('width', def_element_width+'px');
-				$(elements[i]).find('img').not('.portfolio-mp3, .portfolio-loading-bar').each(function(){
+				$(elements[i]).find('img').not('.portfolio-mp3').each(function(){
 				    $(this).addClass('img-type');
 				});
 				var currenthref, elementclass;
@@ -107,42 +106,6 @@ $.fn.multimedia_portfolio = function(options) {
 				}
 		};
 		
-		if (elements.length > settings.nbelem) {
-		      $(".portfolio-container").append("<div class='slider-container' style='left: 66px; width:"+parseInt(settings.width-137)+"px'></div>");
-		      $(".slider-container").append("<div class='ui-slider-1'></div>");
-		      $(".ui-slider-1").css('width', '100%').append("<div class='ui-slider-handle'></div>");
-		      $(".ui-slider-1").slider({steps: elements.length*settings.nbelem, start: 0, slide: function(e,ui) {
-				    mousewheelposition = (elements.length * ui.value /100);
-				    caroussel_portfolio_vue(mousewheelposition, portfolio, elements, settings, ratio_largeur, true);
-		      }});
-		      $(".portfolio-container").mousewheel(function(event, delta) {
-						      if (delta < 0) { mousewheelposition+=.3; if(mousewheelposition>elements.length) mousewheelposition = elements.length;
-						      } else if (delta > 0) { mousewheelposition-=.3; if(mousewheelposition<0) mousewheelposition = 0;
-						      }
-						      caroussel_portfolio_vue(mousewheelposition, portfolio, elements, settings, ratio_largeur, false);
-						      
-						      return false;
-		      }).keypress(function(event) {  
-			      if (event.keyCode == '9') {
-				      return false;
-			      } else if (event.keyCode == '37') {
-				      mousewheelposition-=.3; if(mousewheelposition<0) mousewheelposition = 0;
-				      caroussel_portfolio_vue(mousewheelposition, portfolio, elements, settings, ratio_largeur, false);
-				      
-						     
-			      } else if (event.keyCode == '39') {
-				      mousewheelposition+=.3; if(mousewheelposition>elements.length) mousewheelposition = elements.length;
-				      caroussel_portfolio_vue(mousewheelposition, portfolio, elements, settings, ratio_largeur, false);	     
-			      } 
-		      }).on("swr",function(){
-				      mousewheelposition-=3; if(mousewheelposition<0) mousewheelposition = 0;
-				      caroussel_portfolio_vue_mobile(mousewheelposition, portfolio, elements, settings, ratio_largeur, false);
-		      }).on("swl",function(){
-				      mousewheelposition+=3; if(mousewheelposition>elements.length) mousewheelposition = elements.length;
-				      caroussel_portfolio_vue_mobile(mousewheelposition, portfolio, elements, settings, ratio_largeur, false);	
-		      });
-		}
-		
 		$(".portfolio-img a").fancybox({padding: 1, 'onStart' : function() {$('.flv-type').css('visibility','hidden');}, 'onClosed': function(){caroussel_portfolio_vue(mousewheelposition, portfolio, elements, settings, ratio_largeur, false);}});
 		$(".portfolio-txt a").addClass('fancybox.iframe').fancybox({padding: 1, 'onStart' : function() {$('.flv-type').css('visibility','hidden');}, 'onClosed': function(){caroussel_portfolio_vue(mousewheelposition, portfolio, elements, settings, ratio_largeur, false);}});
 	} else {
@@ -151,50 +114,6 @@ $.fn.multimedia_portfolio = function(options) {
 		$(".content-block").html("<div class='error404'><p class='text-404-first'>Даний додаток не працює при таких параметрах екрану</p><p class='text-404-second'>Мінімальна ширина екрану повинна бути 1024px</p><p class='text-404-threth'>Вибачте за незручність</p><p class='alarm'>Перезавантажте додаток</p></div>")
 	}
 	});
-};
-
-window.onload=function(){
-(function(d){
- var
- ce=function(e,n){var a=document.createEvent("CustomEvent");a.initCustomEvent(n,true,true,e.target);e.target.dispatchEvent(a);a=null;return false},
- nm=true,sp={x:0,y:0},ep={x:0,y:0},
- touch={
-  touchstart:function(e){sp={x:e.touches[0].pageX,y:e.touches[0].pageY}},
-  touchmove:function(e){nm=false;ep={x:e.touches[0].pageX,y:e.touches[0].pageY}},
-  touchend:function(e){if(nm){ce(e,'fc')}else{var x=ep.x-sp.x,xr=Math.abs(x),y=ep.y-sp.y,yr=Math.abs(y);if(Math.max(xr,yr)>20){ce(e,(xr>yr?(x<0?'swl':'swr'):(y<0?'swu':'swd')))}};nm=true},
-  touchcancel:function(e){nm=false}
- };
- for(var a in touch){d.addEventListener(a,touch[a],false);}
-})(document);
-}
-
-function caroussel_portfolio_vue(current, portfolio, elements, settings, ratio_largeur, bslider) {
-	 
-	var decalage = parseInt(settings.width/settings.nbelem*current*ratio_largeur);
-	for ( var i = 0; i < elements.length; i++ ) {
-		$(elements[i]).find('.flv-type, .mp3-type, iframe').each(function() {
-			if ( (((i*settings.width/settings.nbelem)-parseInt(decalage)) < 0) || (((i*settings.width/settings.nbelem)-parseInt(decalage)) > (settings.width-settings.width/settings.nbelem + 26)) ) {
-				$(this).css('visibility','hidden');
-			} else {
-				$(this).css('visibility','visible');}
-		});
-	}
-	portfolio.css('left',(-decalage)+'px');
-	if (!bslider) $('.ui-slider-handle').css('left', parseInt((current/elements.length)*100)+'%');
-};
-function caroussel_portfolio_vue_mobile(current, portfolio, elements, settings, ratio_largeur, bslider) {
-	 
-	var decalage = parseInt(settings.width/settings.nbelem*current*ratio_largeur);
-	for ( var i = 0; i < elements.length; i++ ) {
-		$(elements[i]).find('.flv-type, .mp3-type, iframe').each(function() {
-			if ( (((i*settings.width/settings.nbelem)-parseInt(decalage)) < 0) || (((i*settings.width/settings.nbelem)-parseInt(decalage)) > (settings.width-settings.width/settings.nbelem + 26)) ) {
-				$(this).css('visibility','hidden');
-			} else {
-				$(this).css('visibility','visible');}
-		});
-	}
-	portfolio.animate({'left':(-decalage)+'px'}, 800);
-	if (!bslider) $('.ui-slider-handle').animate({'left': parseInt((current/elements.length)*100)+'%'}, 800);
 };
 
 function convertHex(hex,opacity){
@@ -209,5 +128,4 @@ function convertHex(hex,opacity){
 }
 
 })(jQuery);
-
 
